106d1d26282810a3cf35e7df49901f60d4cc634f
Matt Fujita <matthew.fujita@libertymutual.com>
2017-09-05 14:41:14 -0700

    recipe repo

---
+++ src/main/java/com/libertymutual/goforcode/cookbook/services/RecipeRepository.java
@@ -0,0 +1,15 @@
+package com.libertymutual.goforcode.cookbook.services;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.libertymutual.goforcode.cookbook.models.Recipe;
+
+public interface RecipeRepository {
+	
+	@Repository
+	public interface ActorRepository extends JpaRepository<Recipe, Long> {
+
+	}
+
+}
