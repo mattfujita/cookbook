0993087ebe8c482d6f479cdaa6ef242f374df96e
Matt Fujita <matthew.fujita@libertymutual.com>
2017-09-06 15:00:42 -0700

    fixed swagger

--- src/main/java/com/libertymutual/goforcode/cookbook/config/SwaggerConfig.java
+++ src/main/java/com/libertymutual/goforcode/cookbook/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
 	public Docket apiConfig() {
 		return new Docket(DocumentationType.SWAGGER_2)
 			.select()
-			.apis(RequestHandlerSelectors.basePackage("com.libertymutual.goforcode.wimp"))
+			.apis(RequestHandlerSelectors.basePackage("com.libertymutual.goforcode.cookbook"))
 			.build();
 	}
 }
