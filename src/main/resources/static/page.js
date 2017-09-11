f9432792eef7037703526404a91de1427df0b279
Jon Podosek <jpodosek@gmail.com>
2017-09-11 13:01:02 -0700

    Updated Jon's front end.

--- src/main/resources/static/page.js
+++ src/main/resources/static/page.js
@@ -1,7 +1,64 @@
 const baseurl = 'http://localhost:13000/api/recipes';
 
-$(document).on('click', 'a[data-recipe-id]', function (e) {
+function createListElement(recipe){
+	  $('<li></li>')
+      .html(`
+    		  <a href="#" data-recipe-id="${recipe.id}">
+    		  		${recipe.title}
+    		  </a>
+    		  <form class="delete-recipe-form" method="post" action="/api/recipes/${recipe.id}">
+    		  	<button>Delete</button>
+    		  </form>
+    		  `)
+    		  .appendTo($('#recipe-list'));
+}
+
+$(document).on('submit', '.delete-recipe-form', function (e) {
 	e.preventDefault();
+	
+	//this crafts our own http request of delete
+	$.ajax(this.action, { type: 'DELETE' }) //inline object type: DELETE; if we did a PUT, we'd need a payload
+	.done(() => {
+		$(this) 
+			.closest('li')
+			.remove();
+	})
+	.fail(error => console.error(error));
+});
+
+//capture click event on form
+$('#create-recipe-form').on('submit', function (e) { 
+	e.preventDefault(); //prevent default action on button click
+	console.log(e);
+	
+	//define a brand new object
+	let payload = {
+		title: $('#title').val(),
+		description: $('#description').val(),
+		minutes: $('#minutes').val(),
+		pictureURL: $('#pictureURL').val()
+	};
+	//console.log(payload); //validate
+
+	let ajaxArguments = {
+	type: 'POST', //http method
+	data: JSON.stringify(payload), //convert payload object to string
+	contentType: 'application/json' //telling server we're passing JSON changes way server behaves when getting info
+	};
+	
+	$.ajax(this.action, ajaxArguments)
+	//equivalent to below function declaration .done((card)) => 
+    .done(function (recipe) {
+    	createListElement(recipe);
+      
+    })
+    //arrow function syntax
+    .fail(error => console.error(error)); 
+	
+});
+
+$(document).on('click', 'a[data-recipe-id]', function (e) {
+	e.preventDefault(); 
 	const recipeId = $(this).data('recipeId');
 	
 	$.getJSON(baseurl + '/' + recipeId, function(data) {
@@ -13,6 +70,7 @@ $(document).on('click', 'a[data-recipe-id]', function (e) {
 					<h1>${data.title}</h1>
 					<h2>${data.description}</h2>
 					<div>${data.minutes}</div>
+					<div><img src=${data.pictureURL} /></div>
 					<div>${data.ingredients}</div>
 					<div>${data.instructions}</div>
 			`);
@@ -24,9 +82,7 @@ $.getJSON(baseurl, function (data) {
 	console.log('I got data:', data);
 	if(data.length) {
 		for(let recipe of data) {
-			$('<li></li>')
-			.html('<a href="#" data-recipe-id="' + recipe.id + '">' + recipe.title + '</a>')
-			.appendTo($('#recipe-list'));
+			createListElement(recipe);
 		}
 	} else {
 		$('<li></li>')
