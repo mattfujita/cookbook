52d72d4a0c5ee6696bdc367e39e2b6d0064bce77
Jon Podosek <jpodosek@gmail.com>
2017-09-11 15:26:06 -0700

    Working on delete for instruction.

--- src/main/resources/static/page.js
+++ src/main/resources/static/page.js
@@ -1,5 +1,6 @@
 const baseurl = 'http://localhost:13000/api/recipes';
 
+//Create Recipe element
 function createListElement(recipe){
 	  $('<li></li>')
       .html(`
@@ -13,20 +14,71 @@ function createListElement(recipe){
     		  .appendTo($('#recipe-list'));
 }
 
-$(document).on('submit', '.delete-recipe-form', function (e) {
-	e.preventDefault();
+function fillInDetails(data) {
 	
-	//this crafts our own http request of delete
-	$.ajax(this.action, { type: 'DELETE' }) //inline object type: DELETE; if we did a PUT, we'd need a payload
-	.done(() => {
-		$(this) 
-			.closest('li')
-			.remove();
-	})
-	.fail(error => console.error(error));
-});
+	let html = `
+		<h1>${data.title}</h1>
+		<h2>${data.description}</h2>
+		<div>Minutes: ${data.minutes}</div>
+	`;
+	
+	for (let instruction of data.instructions) {
+		$('<li></li>')
+		html += `
+			<div>
+				<b>${instruction.stepNumber}</b>
+				<div>${instruction.instructionText}<div>
+				
+			<form class="delete-instruction-form" method="post" action="/api/${recipe.id}/instructions/${instruction.id}">
+    		  	<button>Delete</button>
+    		</form>
+    		  </div>	
+		`;
+	}
+	
+	html += `
+		
+	<form id="create-instruction-form" method="post" action="api/recipes/${data.id}/instructions">
+		Step Number:<input required name="stepNumber" id="stepNumber" placeholder="stepNumber">
+		<br>
+		Instruction Text:<input required name="instructionText" id="instructionText" placeholder="instructionText">
+		<br><br>
+		<button>Add instruction</button>
+	</form><br><br>
+	`;
+	
+	
+	for (let ingredient of data.ingredients) {
+		html += `
+			<div>
+				<b>${ingredient.ingredientName}</b>
+				<div>${ingredient.measureUnit}<div>
+				<div>${ingredient.ingredientQuantity}<div>
+			</div>
+		`;
+	}
+	
+	html += `
+		
+	<form id="create-ingredient-form" method="post" action="api/recipes/${data.id}/ingredients">
+		Ingredient Name: <input required name="ingredientName" id="ingredientName" placeholder="ingredientName">
+		<br>
+		Unit of Measurement:<input required name="measureUnit" id="measureUnit" placeholder="measureUnit">
+		<br>
+		Quantity:<input required name="ingredientQuantity" id="ingredientQuantity" placeholder="ingredientQuantity">
+		<br><br>
+		<button>Add ingredient</button>
+	</form>
+	`;
 
-//capture click event on form
+	$('#recipe-detail').html(html);
+	
+}
+
+
+
+
+//Create recipe
 $('#create-recipe-form').on('submit', function (e) { 
 	e.preventDefault(); //prevent default action on button click
 	console.log(e);
@@ -57,24 +109,107 @@ $('#create-recipe-form').on('submit', function (e) {
 	
 });
 
+//delete recipe
+$(document).on('submit', '.delete-recipe-form', function (e) {
+	e.preventDefault();
+	
+	//this crafts our own http request of delete
+	$.ajax(this.action, { type: 'DELETE' }) //inline object type: DELETE; if we did a PUT, we'd need a payload
+	.done(() => {
+		$(this) //form element
+			.closest('li') //traverse up html tree to find closest li tag
+			.remove();
+	})
+	.fail(error => console.error(error));
+});
+
+
+
+//Create instruction
+$(document).on('submit', '#create-instruction-form', function (e) {
+	e.preventDefault();
+	//create payload object
+	let payload = {
+		stepNumber: $('#stepNumber').val(),
+		instructionText: $('#instructionText').val()
+			
+	};
+	
+	//create ajaxArgs object
+	let ajaxArguments = {
+		type: 'POST',
+		data: JSON.stringify(payload),
+		contentType: 'application/json'		
+	};
+	
+	
+	$.ajax(this.action, ajaxArguments)
+	//equivalent to below function declaration .done((card)) => 
+    .done(function (instruction) {
+    	fillInDetails(instruction);
+      console.log(instruction);
+    })
+  //arrow function syntax
+    .fail(error => console.error(error)); 
+});
+
+//delete instruction
+$(document).on('submit', '.delete-instruction-form', function (e) {
+	e.preventDefault();
+	
+	//this crafts our own http request of delete
+	$.ajax(this.action, { type: 'DELETE' }) //inline object type: DELETE; if we did a PUT, we'd need a payload
+	.done(() => {
+		$(this) //form element
+			.closest('li') //traverse up html tree to find closest li tag
+			.remove();
+	})
+	.fail(error => console.error(error));
+});
+
+//Create ingredient
+$(document).on('submit', '#create-ingredient-form', function (e) {
+	e.preventDefault();
+	//create payload object
+	let payload = {
+		ingredientName: $('#ingredientName').val(),
+		measureUnit: $('#measureUnit').val(),
+		ingredientQuantity: $('#ingredientQuantity').val()
+			
+	};
+	
+	//create ajaxArgs object
+	let ajaxArguments = {
+		type: 'POST',
+		data: JSON.stringify(payload),
+		contentType: 'application/json'		
+	};
+	
+	
+	$.ajax(this.action, ajaxArguments)
+    .done(function (ingredient) {
+    	fillInDetails(ingredient);
+      console.log(ingredient);
+    })
+  //arrow function syntax
+    .fail(error => console.error(error)); 
+});
+
+		
+
+//show details when clicking on recipe link
 $(document).on('click', 'a[data-recipe-id]', function (e) {
 	e.preventDefault(); 
 	const recipeId = $(this).data('recipeId');
+	
 	
 	$.getJSON(baseurl + '/' + recipeId, function(data) {
 		console.log('Data for', recipeId, 'is', data);
 		data.ingredients = data.ingredients || '';
 		data.instructions = data.instructions || '';
-		$('#recipe-detail')
-			.html(`
-					<h1>${data.title}</h1>
-					<h2>${data.description}</h2>
-					<div>${data.minutes}</div>
-					<div><img src=${data.pictureURL} /></div>
-					<div>${data.ingredients}</div>
-					<div>${data.instructions}</div>
-			`);
-			
+		
+	fillInDetails(data);
+				
 	});
 });
 
